af144c35b271c178ceec213ce8a42fb045f45763
Paco <francisco.albinana@iesnervion.es>
2022-10-04 13:25:06 +0200

    04/10

--- src/Main3.java
+++ src/Main3.java
@@ -1,16 +1,25 @@
+
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main3 {
 
     public static void main(String[] args) {
 
-        String [] comando = new String[];
+        Scanner sc = new Scanner(System.in);
 
-        comando =
+        String [] comandos;
 
-        ProcessBuilder pb = new ProcessBuilder(args);
+        String comando = sc.nextLine();
+
+        //String comando = JOptionPane.showInputDialog(System.in);
+
+        comandos = comando.split(" ");
+
+
+        ProcessBuilder pb = new ProcessBuilder(comandos);
         File directorio = new File("C:\\Users\\falbinana\\IdeaProjects\\ProcesosTema1\\src\\Ejercicios");
         pb.directory(directorio);
         pb.inheritIO();
